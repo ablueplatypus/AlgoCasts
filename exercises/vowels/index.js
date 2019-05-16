7aed327859a96b45b89da97cf91d68812e3258c3
Matt Doering <ablueplatypus21@gmail.com>
2019-05-16 10:38:52 -0400

    added vowels answer

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,16 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let count = 0;
+  const checking = ['a', 'i', 'o', 'u']
+  for (let char of str.toLowerCase()) {
+     if(checking.includes(char)){
+      count++
+     }
+    }
+    return count;
+  }
+
 
 module.exports = vowels;
